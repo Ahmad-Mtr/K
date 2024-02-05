2b9bfabf23cfca322696f1646b657b75541c1859
AhmadMtr <70106976+Ahmad-Mtr@users.noreply.github.com>
2024-02-05 14:28:43 +0300

    tmp commit

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,9 +1,9 @@
 class AppwriteConstants {
-  static const String databaseId = 'X_clone_db';
-  static const String projectId = '65906e300bc24439ea4f';
+  static const String databaseId = '65ba0b435be0fb9e7272';
+  static const String projectId = '65ba098d287a4b2cb8bd';
   static const String endPoint = 'https://cloud.appwrite.io/v1';
 
-  static const String usersCollection = '65b8c0959ffbc1fa9477';
+  static const String usersCollection = '65ba0bb5a6d6d3ad518e';
   static const String tweetsCollection = '';
   static const String notificationsCollection = '';
 
